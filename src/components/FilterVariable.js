8d1274491b0a93033ee22b90e926cde5b56c4a7b
hafen <rhafen@gmail.com>
2019-02-06 18:41:05 -0800

    Add clickaway for variable filter

--- src/components/FilterVariable.js
+++ src/components/FilterVariable.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { setFilters } from '../actions';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import { setFilters, setFilterOpen } from '../actions';
 import { ui } from '../constants';
 
 const FilterVariable = ({
-  windowSize, filters, filterOpen, data, toggleFilters
+  windowSize, filters, filterOpen, data, toggleFilters, closeFilter
 }) => {
   if (filterOpen === false || data.ogm === undefined) {
     return '';
@@ -13,52 +14,54 @@ const FilterVariable = ({
 
   // https://www.npmjs.com/package/react-click-outside
   return (
-    <div className="filters-container" style={{ width: windowSize.appWidth, top: ui.header.height, left: windowSize.appLeft }}>
-      <div className="filter-column">
-        <div className="filter-column-header">
-          <span>Organogenesis, Growth, & Maturation</span>
+    <ClickAwayListener onClickAway={closeFilter}>
+      <div className="filters-container" style={{ width: windowSize.appWidth, top: ui.header.height, left: windowSize.appLeft }}>
+        <div className="filter-column">
+          <div className="filter-column-header">
+            <span>Organogenesis, Growth, & Maturation</span>
+          </div>
+          <div className="filter-column-content">
+            { Object.keys(data.ogm.data).map(d => (
+              <div
+                key={d}
+                className={`filter-button ${filters.ogm.indexOf(d) > -1 ? 'selected' : 'unselected'}`}
+                onClick={() => { toggleFilters(d, 'ogm'); }}
+                onKeyPress={() => {}}
+                role="button"
+                tabIndex="-10"
+              >
+                {d}
+              </div>
+            )) }
+          </div>
         </div>
-        <div className="filter-column-content">
-          { Object.keys(data.ogm.data).map(d => (
-            <div
-              key={d}
-              className={`filter-button ${filters.ogm.indexOf(d) > -1 ? 'selected' : 'unselected'}`}
-              onClick={() => { toggleFilters(d, 'ogm'); }}
-              onKeyPress={() => {}}
-              role="button"
-              tabIndex="-10"
-            >
-              {d}
-            </div>
-          )) }
+        <div className="filter-column">
+          <div className="filter-column-header">
+            <span>Neurodevelopment</span>
+          </div>
+          <div className="filter-column-content">
+            { Object.keys(data.nd.data).map(d => (
+              <div
+                key={d}
+                className={`filter-button ${filters.nd.indexOf(d) > -1 ? 'selected' : 'unselected'}`}
+                onClick={() => { toggleFilters(d, 'nd'); }}
+                onKeyPress={() => {}}
+                role="button"
+                tabIndex="-10"
+              >
+                {d}
+              </div>
+            )) }
+          </div>
+        </div>
+        <div className="filter-column">
+          <div className="filter-column-header">
+            <span>Outcomes, Risk Factors, & Interventions</span>
+          </div>
+          <div className="filter-column-content" />
         </div>
       </div>
-      <div className="filter-column">
-        <div className="filter-column-header">
-          <span>Neurodevelopment</span>
-        </div>
-        <div className="filter-column-content">
-          { Object.keys(data.nd.data).map(d => (
-            <div
-              key={d}
-              className={`filter-button ${filters.nd.indexOf(d) > -1 ? 'selected' : 'unselected'}`}
-              onClick={() => { toggleFilters(d, 'nd'); }}
-              onKeyPress={() => {}}
-              role="button"
-              tabIndex="-10"
-            >
-              {d}
-            </div>
-          )) }
-        </div>
-      </div>
-      <div className="filter-column">
-        <div className="filter-column-header">
-          <span>Outcomes, Risk Factors, & Interventions</span>
-        </div>
-        <div className="filter-column-content" />
-      </div>
-    </div>
+    </ClickAwayListener>
   );
 };
 
@@ -67,8 +70,8 @@ FilterVariable.propTypes = {
   filters: PropTypes.object.isRequired,
   filterOpen: PropTypes.bool.isRequired,
   data: PropTypes.object.isRequired,
-  toggleFilters: PropTypes.func.isRequired
-  // closeFilter: PropTypes.func.isRequired
+  toggleFilters: PropTypes.func.isRequired,
+  closeFilter: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -81,10 +84,10 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   toggleFilters: (val, group) => {
     dispatch(setFilters({ val, group, type: 'toggle' }));
+  },
+  closeFilter: () => {
+    dispatch(setFilterOpen(false));
   }
-  // closeFilter: () => {
-  //   dispatch(setFilterOpen(false));
-  // }
 });
 
 export default connect(
