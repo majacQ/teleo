4139bcee311085bd5bb94b649e27d1c5677127d1
hafen <rhafen@gmail.com>
2019-02-06 18:27:03 -0800

    Add material ui and animate event detail

--- src/components/ExpandInfo.js
+++ src/components/ExpandInfo.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Collapse from '@material-ui/core/Collapse';
 import { niceAge } from '../utils/ageCalc';
 import NetworkGraph from './NetworkGraph';
 // import { CSSTransition } from 'react-transition-group';
@@ -8,58 +9,60 @@ import NetworkGraph from './NetworkGraph';
 const ExpandInfo = ({
   data, windowSize
 }) => {
+  const [expand, setExpand] = useState(false);
+
+  useEffect(() => {
+    setExpand(true);
+  });
+
   const focWidth = windowSize.appWidth;
 
-  // <CSSTransition
-  //   timeout={300}
-  //   classNames="expand-info"
-  //   onEnter={() => alert('hi')}
-  // >
-
   return (
-    <div className="expand-info-wrapper" style={{ width: windowSize.width }}>
-      <div className="expand-info" style={{ width: focWidth, marginLeft: windowSize.appLeft }}>
-        <div className="expand-info-header">{data.desc_short}</div>
-        <div className="expand-info-age-container">
-          <div className="expand-info-age-label">
-            Start age
+    <Collapse in={expand}>
+      <div className="expand-info-wrapper" style={{ width: windowSize.width }}>
+        <div className="expand-info" style={{ width: focWidth, marginLeft: windowSize.appLeft }}>
+          <div className="expand-info-header">{data.desc_short}</div>
+          <div className="expand-info-age-container">
+            <div className="expand-info-age-label">
+              Start age
+            </div>
+            <div className="expand-info-age">
+              {niceAge(data.age_start)}
+            </div>
           </div>
-          <div className="expand-info-age">
-            {niceAge(data.age_start)}
+          <div className="expand-info-age-container">
+            <div className="expand-info-age-label">
+              Peak start
+            </div>
+            <div className="expand-info-age">
+              {data.age_start_peak ? niceAge(data.age_start_peak) : 'n/a'}
+            </div>
           </div>
+          <div className="expand-info-age-container">
+            <div className="expand-info-age-label">
+              Peak end
+            </div>
+            <div className="expand-info-age">
+              {data.age_end_peak ? niceAge(data.age_end_peak) : 'n/a'}
+            </div>
+          </div>
+          <div className="expand-info-age-container">
+            <div className="expand-info-age-label">
+              End age
+            </div>
+            <div className="expand-info-age">
+              {niceAge(data.age_end)}
+            </div>
+          </div>
+          <div className="expand-info-hline" />
+          <div className="expand-info-detail">
+            { data.class !== undefined && <NetworkGraph data={data} /> }
+            { data.class === undefined && data.desc_long}
+          </div>
+          <div className="expand-info-hline" />
         </div>
-        <div className="expand-info-age-container">
-          <div className="expand-info-age-label">
-            Peak start
-          </div>
-          <div className="expand-info-age">
-            {data.age_start_peak ? niceAge(data.age_start_peak) : 'n/a'}
-          </div>
-        </div>
-        <div className="expand-info-age-container">
-          <div className="expand-info-age-label">
-            Peak end
-          </div>
-          <div className="expand-info-age">
-            {data.age_end_peak ? niceAge(data.age_end_peak) : 'n/a'}
-          </div>
-        </div>
-        <div className="expand-info-age-container">
-          <div className="expand-info-age-label">
-            End age
-          </div>
-          <div className="expand-info-age">
-            {niceAge(data.age_end)}
-          </div>
-        </div>
-        <div className="expand-info-hline" />
-        <div className="expand-info-detail">
-          { data.class !== undefined && <NetworkGraph data={data} /> }
-          { data.class === undefined && data.desc_long}
-        </div>
-        <div className="expand-info-hline" />
       </div>
-    </div>
+    </Collapse>
   );
 };
 
