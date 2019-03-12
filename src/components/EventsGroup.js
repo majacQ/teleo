b1a9d2a6f09386db8bf2dc1deba2a76af857e64b
hafen <rhafen@gmail.com>
2019-03-12 16:35:37 -0700

    Fix event groups to expand/collapse correctly

--- src/components/EventsGroup.js
+++ src/components/EventsGroup.js
@@ -5,10 +5,18 @@ import EventsList from './EventsList';
 import { setFilters, setSelectedORFI, setCollapsedGroup } from '../actions';
 
 const EventsGroup = ({
-  windowSize, collapsedGroups, removeGroup, toggleCollapse, data, subcategory, category, gid, group
+  windowSize, collapsedGroups, filters, selectedORFI, removeGroup,
+  toggleCollapse, data, subcategory, category, gid, group
 }) => {
   const focWidth = windowSize.appWidth;
   const isCollapsed = collapsedGroups.indexOf(gid) > -1;
+
+  const nGroups = filters.ogm.length + filters.nd.length
+    + (selectedORFI.ho.length > 0 ? 1 : 0)
+    + (selectedORFI.rf.length > 0 ? 1 : 0)
+    + (selectedORFI.int.length > 0 ? 1 : 0);
+  const draggable = nGroups === collapsedGroups.length;
+
   return (
     <div style={{ width: windowSize.width }}>
       <svg width={windowSize.width} height="25">
@@ -20,7 +28,7 @@ const EventsGroup = ({
           <span className="eventgroup-cat">{category}</span>
         </div>
         <div className="eventgroup-header-icons">
-          <span className="icon-drag_handle eventgroup-header-icon" style={{ color: 'lightgray' }} />
+          {draggable && (<span className="icon-drag_handle eventgroup-header-icon" style={{ color: 'lightgray' }} />) }
           <span
             className={`icon-chevron-${isCollapsed ? 'up' : 'down'} eventgroup-header-icon`}
             onClick={() => { toggleCollapse(gid); }}
@@ -45,6 +53,8 @@ const EventsGroup = ({
 EventsGroup.propTypes = {
   windowSize: PropTypes.object.isRequired,
   collapsedGroups: PropTypes.array.isRequired,
+  filters: PropTypes.object.isRequired,
+  selectedORFI: PropTypes.object.isRequired,
   removeGroup: PropTypes.func.isRequired,
   toggleCollapse: PropTypes.func.isRequired,
   data: PropTypes.array.isRequired,
@@ -56,7 +66,9 @@ EventsGroup.propTypes = {
 
 const mapStateToProps = state => ({
   windowSize: state.windowSize,
-  collapsedGroups: state.collapsedGroups
+  collapsedGroups: state.collapsedGroups,
+  filters: state.filters,
+  selectedORFI: state.selectedORFI
 });
 
 const mapDispatchToProps = dispatch => ({
